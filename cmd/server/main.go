70599c68e00a14e9080f03fc65b1b950a914e84b
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-02-13 21:06:37 -0600

    Added CORS headers to responses.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Mr-Rafael/finance-calculator/internal/handlers"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	port := ":8080"
 	mux := http.NewServeMux()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error reading .env: %v", err)
+		return
+	}
 
 	var config handlers.ApiConfig
 	config.FileserverHits.Store(0)
@@ -23,11 +30,26 @@ func main() {
 
 	server := &http.Server{
 		Addr:    port,
-		Handler: mux,
+		Handler: withCORS(mux),
 	}
 
 	fmt.Printf("Starting server on %v\n", port)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("server error: %v", err)
 	}
+}
+
+func withCORS(h http.Handler) http.Handler {
+	allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
