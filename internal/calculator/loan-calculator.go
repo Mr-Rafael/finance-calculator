a27e075d8cd251d7eed55c0f532645f0dda4d63a
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-02-24 21:57:06 -0600

    Implemented additional error messages for the loan calculator.

--- internal/calculator/loan-calculator.go
+++ internal/calculator/loan-calculator.go
@@ -14,6 +14,9 @@ const minInterestRate = "0"
 const maxInterestRate = "100"
 const minMonthlyPaymentCents = "1"
 const maxMonthlyPaymentCents = "100000000000"
+const minEscrowCents = "0"
+const maxEscrowCents = "100000000000"
+const maxPaymentYears = 30
 
 type LoanInfo struct {
 	startingPrincipal   decimal.Decimal
@@ -46,6 +49,10 @@ func getLoanInfoFromRequest(request models.LoanRequestParams) (LoanInfo, error) 
 	}
 
 	info.escrowPayment = decimal.NewFromInt(int64(request.EscrowPayment))
+	if !decimalIsBetween(info.escrowPayment, minEscrowCents, maxEscrowCents) {
+		return LoanInfo{}, fmt.Errorf("invalid escrow payment: '%v'. the accepted range is 0.01 - 1,000,000,000", info.escrowPayment.Div(decimal.NewFromInt(100)).Round(2))
+	}
+
 	startDate, err := time.Parse("2006-01-02", request.StartDate)
 	if err != nil {
 		return LoanInfo{}, fmt.Errorf("invalid start date: %v", request.StartDate)
@@ -67,7 +74,13 @@ func CalculateLoanPaymentPlan(info models.LoanRequestParams) (models.LoanPayment
 	totalExpenditure := decimal.NewFromInt(0)
 	i := 0
 
-	for currentPrincipal.Compare(decimal.NewFromInt(0)) == 1 && i < 360 {
+	isPaymentEnough, minPayment := loanInfo.isPaymentEnough()
+	if !isPaymentEnough {
+		return models.LoanPaymentPlan{}, fmt.Errorf("entered payment amount does not cover the first month's interest and expenditures (%v). please enter a higher payment amount",
+			minPayment.Div(decimal.NewFromInt(100)).Round(2).IntPart())
+	}
+
+	for currentPrincipal.Compare(decimal.NewFromInt(0)) == 1 && i < (maxPaymentYears*12) {
 		currentPayment := loanInfo.monthlyPayment
 		currentInterest := currentPrincipal.Mul(loanInfo.monthlyInterestRate)
 		currentExpenditure := currentInterest.Add(loanInfo.escrowPayment)
@@ -91,9 +104,21 @@ func CalculateLoanPaymentPlan(info models.LoanRequestParams) (models.LoanPayment
 		}
 		plan.Plan = append(plan.Plan, currentStatus)
 	}
+	if currentPrincipal.GreaterThan(decimal.Zero) {
+		return models.LoanPaymentPlan{}, fmt.Errorf("loan term surpasses the accepted limit (%v years), with a remaining %v principal. please enter a higher monthly payment.",
+			maxPaymentYears,
+			currentPrincipal.Round(0).IntPart())
+	}
+
 	plan.DurationMonths = i
 	plan.TotalExpenditure = int(totalExpenditure.Round(0).IntPart())
 	plan.TotalPaid = int(totalPaid.Round(0).IntPart())
 	plan.CostOfCreditPercent = getReturnPercent(totalPaid.Div(loanInfo.startingPrincipal))
 	return plan, nil
+}
+
+func (info LoanInfo) isPaymentEnough() (bool, decimal.Decimal) {
+	minFirstPayment := info.startingPrincipal.Mul(info.monthlyInterestRate).Add(info.escrowPayment)
+	isEnough := !minFirstPayment.GreaterThanOrEqual(info.monthlyPayment)
+	return isEnough, minFirstPayment
 }
