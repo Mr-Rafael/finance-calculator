021e0e90b1b549327f65344d4782757ac163a912
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-03-07 21:48:46 -0600

    Added Access-Control-Allow-Credentials header to allow the frontend to send credentials.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -69,6 +69,7 @@ func withCORS(h http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
