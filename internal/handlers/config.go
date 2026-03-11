3b06f20bbce6fa07b1ada9765e13fc3241f8d19b
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-03-10 22:05:01 -0600

    Added an authorization middleware to protect the loan and savings endpoints.

--- internal/handlers/config.go
+++ internal/handlers/config.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
 
 	"github.com/Mr-Rafael/finance-calculator/internal/db"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type ApiConfig struct {
@@ -17,6 +20,36 @@ type ApiConfig struct {
 func (cfg *ApiConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.FileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *ApiConfig) AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader == "" {
+			respondWithErrorCode(w, "missing authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+
+			return []byte(cfg.AccessSecret), nil
+		})
+
+		if err != nil || !token.Valid {
+			respondWithErrorCode(w, "expired token", http.StatusUnauthorized)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
