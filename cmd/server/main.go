bb59486f7a174e9c456f1b0c8d230c55112e4d5a
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-03-11 21:26:43 -0600

    Added Authorization to the Access-Control-Allow-Headers CORS response.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -68,7 +68,7 @@ func withCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
