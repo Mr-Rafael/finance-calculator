e8f73d3d570caeb92c35a803a9c7801a4b772d9f
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-04-14 21:19:48 -0600

    Added a unit test for loan creation.

--- internal/repository/loans_repo_test.go
+++ internal/repository/loans_repo_test.go
@@ -59,7 +59,7 @@ func TestSaveLoanPaymentPlan(t *testing.T) {
 
 	got, err := repo.SaveLoanPaymentPlan(ctx, params)
 	if err != nil {
-		log.Fatalf("Error saving the refresh token in database: %v", err)
+		log.Fatalf("Error saving loan in database: %v", err)
 	}
 
 	want := db.Loan{
@@ -71,5 +71,70 @@ func TestSaveLoanPaymentPlan(t *testing.T) {
 
 	if got.UserID.Bytes != want.UserID.Bytes {
 		log.Fatalf("Saved (%v) and expected (%v) user IDs did not match.", got.UserID.Bytes, want.UserID.Bytes)
+	}
+}
+
+func TestGetLoanPaymentPlan(t *testing.T) {
+	ctx := context.Background()
+	queries := initializeQueries(ctx)
+	repo := NewLoansRepo(queries)
+
+	test_user_id, err := uuid.Parse("af38df43-3ced-4869-9930-93a0fa0cf1e0")
+	if err != nil {
+		log.Fatalf("failed to parse the test user uuid: %v", err)
+	}
+
+	originalData := dto.LoanRequestParams{
+		StartingPrincipal:  0,
+		YearlyInterestRate: "0",
+		MonthlyPayment:     0,
+		EscrowPayment:      0,
+		StartDate:          "1970-01-01",
+	}
+	status := domain.LoanStatus{
+		Date:          time.Now(),
+		Payment:       decimal.Zero,
+		Interest:      decimal.Zero,
+		OtherPayments: decimal.Zero,
+		Paydown:       decimal.Zero,
+		Principal:     decimal.Zero,
+	}
+	params := domain.LoanPaymentPlan{
+		ID:                  uuid.Nil,
+		UserID:              test_user_id,
+		Name:                "test",
+		OriginalData:        domain.LoansInput(originalData),
+		StartingPrincipal:   decimal.Zero,
+		CurrentPrincipal:    decimal.Zero,
+		InterestMultiplierM: decimal.Zero,
+		PaymentM:            decimal.Zero,
+		EscrowM:             decimal.Zero,
+		Date:                time.Now(),
+		DurationMonths:      0,
+		TotalExpenditure:    decimal.Zero,
+		TotalPaid:           decimal.Zero,
+		CostOfCreditPercent: decimal.Zero,
+	}
+	params.Plan = append(params.Plan, status)
+
+	plan, err := repo.SaveLoanPaymentPlan(ctx, params)
+	if err != nil {
+		log.Fatalf("Error saving loan in database: %v", err)
+	}
+
+	got, err := repo.GetLoanByID(ctx, plan.ID.Bytes, plan.UserID.Bytes)
+	if err != nil {
+		log.Fatalf("Error getting loan from database: %v", err)
+	}
+
+	want := db.Loan{
+		UserID: pgtype.UUID{
+			Bytes: test_user_id,
+			Valid: true,
+		},
+	}
+
+	if got.UserID != want.UserID.Bytes {
+		log.Fatalf("The created loan and the retrieved loan didn't match")
 	}
 }
