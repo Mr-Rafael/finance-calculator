b86efb69eeeca5ef3796c6e6f29a2dd4190b86cc
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-04-21 20:46:07 -0600

    Implemented unit tests for the auth validate function.

--- internal/service/auth_service_test.go
+++ internal/service/auth_service_test.go
@@ -47,3 +47,74 @@ func TestValidateAccessToken(t *testing.T) {
 		log.Fatalf("The expected User ID (%v) did not match the one obtained from validation (%v)", want, got)
 	}
 }
+
+func TestValidateExpiredToken(t *testing.T) {
+	mockAccessSecret := "ACCESS"
+	mockRefreshSecret := "REFRESH"
+	mockAuthRepo := &MockAuthRepo{
+		CreateRefreshTokenFunc: nil,
+	}
+	mockUsersRepo := &MockUsersRepo{
+		CreateUserFunc: nil,
+	}
+	service := NewAuthService(mockAuthRepo, mockUsersRepo, mockAccessSecret, mockRefreshSecret)
+
+	userID := "001"
+	claims := auth.AccessClaims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(-1 * time.Second)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "savings-app",
+			Subject:   userID,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(mockAccessSecret))
+	if err != nil {
+		log.Fatalf("Failed to generate test access token: %v", err)
+	}
+
+	_, err = service.ValidateAccessToken(signedToken)
+	if err == nil {
+		log.Fatalf("Validate function returned the expired token as valid.")
+	}
+}
+
+func TestValidateInvalidSignature(t *testing.T) {
+	mockAccessSecret := "ACCESS"
+	badAccessSecret := "NOACCESS"
+	mockRefreshSecret := "REFRESH"
+	mockAuthRepo := &MockAuthRepo{
+		CreateRefreshTokenFunc: nil,
+	}
+	mockUsersRepo := &MockUsersRepo{
+		CreateUserFunc: nil,
+	}
+	service := NewAuthService(mockAuthRepo, mockUsersRepo, mockAccessSecret, mockRefreshSecret)
+
+	userID := "001"
+	claims := auth.AccessClaims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(-1 * time.Second)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "savings-app",
+			Subject:   userID,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(badAccessSecret))
+	if err != nil {
+		log.Fatalf("Failed to generate test access token: %v", err)
+	}
+
+	_, err = service.ValidateAccessToken(signedToken)
+	if err == nil {
+		log.Fatalf("Validate function wrongly signed token as valid.")
+	}
+}
