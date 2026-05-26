ebc54222bb3278b4e8029a77b48c5460c24eb3ed
Mr-Rafael <rafael.mazariegosr@gmail.com>
2026-05-25 20:29:02 -0600

    Implemented unit tests for user handler layer.

---
+++ internal/api/users_handlers_test.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/Mr-Rafael/finance-calculator/internal/db"
+	"github.com/Mr-Rafael/finance-calculator/internal/service"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUser(t *testing.T) {
+	mockUsersRepo := &service.MockUsersRepo{
+		CreateUserFunc: func(ctx context.Context, params db.CreateUserParams) (db.User, error) {
+			return db.User{}, nil
+		},
+	}
+	service := service.NewUserService(mockUsersRepo)
+	handler := NewUsersHandler(service)
+
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/app/savings/calculate",
+		strings.NewReader(`{
+			"email":    "test@user.com",
+			"password": "password",
+			"username": "username"
+		}`),
+	)
+	rr := httptest.NewRecorder()
+
+	handler.CreateUser(rr, req)
+
+	require.Equal(t, http.StatusCreated, rr.Code)
+}
+
+func TestCreateUserError(t *testing.T) {
+	mockUsersRepo := &service.MockUsersRepo{
+		CreateUserFunc: func(ctx context.Context, params db.CreateUserParams) (db.User, error) {
+			return db.User{}, fmt.Errorf("Error saving user")
+		},
+	}
+	service := service.NewUserService(mockUsersRepo)
+	handler := NewUsersHandler(service)
+
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/app/savings/calculate",
+		strings.NewReader(`{
+			"email":    "test@user.com",
+			"password": "password",
+			"username": "username"
+		}`),
+	)
+	rr := httptest.NewRecorder()
+
+	handler.CreateUser(rr, req)
+
+	require.Equal(t, http.StatusInternalServerError, rr.Code)
+}
